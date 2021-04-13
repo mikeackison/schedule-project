bbbb273d3ec6bd036ae845f8b5120030718383bf
Mike Ackison <mike.ackison@shopify.com>
2021-04-13 18:50:27 -0400

    fixed formatting

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
-import "components/Application.scss";
+import 'components/Application.scss';
 
 export default function Application(props) {
   return (
-    <main className="layout">
-      <section className="sidebar">
+    <main className='layout'>
+      <section className='sidebar'>
         <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
+          className='sidebar--centered'
+          src='images/logo.png'
+          alt='Interview Scheduler'
         />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <hr className='sidebar__separator sidebar--centered' />
+        <nav className='sidebar__menu'></nav>
         <img
-          className="sidebar__lhl sidebar--centered"
+          className='sidebar__lhl sidebar--centered'
           // src="images/lhl.png"
           // alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
+      <section className='schedule'>
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
