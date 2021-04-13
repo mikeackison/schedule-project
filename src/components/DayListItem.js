6b2c2010014025c28f9d0fb6d2376f0b10167cd7
Mike Ackison <mike.ackison@shopify.com>
2021-04-13 18:45:30 -0400

    add stying for dayListItem, as well as logic for for spots remaining, tests passing, storybook works

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "components/DayListItem.scss";
+
+const classNames = require("classnames");
+
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return `no spots remaining`
+    } else if (props.spots === 1) {
+      return `${props.spots} spot remaining`
+    } else if (props.spots >= 2 ) {
+      return `${props.spots} spots remaining`
+    }
+  };
+
+
+  return (
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
