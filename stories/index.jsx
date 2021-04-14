7323c21c7ecf1edf628ae377fe7e887e2ae53e72
Mike Ackison <mike.ackison@shopify.com>
2021-04-14 18:56:12 -0400

    index stories working now, main page working after moving scss to styles folder

--- stories/index.jsx
+++ stories/index.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import 'index.scss';
+import 'styles/index.scss';
 
 import Button from 'components/Button';
 import DayListItem from 'components/DayListItem';
+
 
 storiesOf('Button', module)
   .addParameters({
