3a5ab1c815f04ab72f7976b94c0df81dac7c4219
Mike Ackison <mike.ackison@shopify.com>
2021-04-14 19:21:28 -0400

    mock data on sidebar working

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import DayList from 'components/DayList'
 
 import 'styles/Application.scss';
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
@@ -12,12 +31,14 @@ export default function Application(props) {
           alt='Interview Scheduler'
         />
         <hr className='sidebar__separator sidebar--centered' />
-        <nav className='sidebar__menu'></nav>
-        <img
-          className='sidebar__lhl sidebar--centered'
-          // src="images/lhl.png"
-          // alt="Lighthouse Labs"
+        <nav className='sidebar__menu'>
+
+        <DayList
+          days={days}
+          day={"Monday"}
+          setDay={day => console.log(day)}
         />
+        </nav>
       </section>
       <section className='schedule'>
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
