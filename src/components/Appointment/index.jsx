fcaf95a1fb02687c12ba0523f6c3a141f48878c1
Mike Ackison <mike.ackison@shopify.com>
2021-04-15 19:56:06 -0400

    fixed bug with extra space in Appoinment folder, stories working now

---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'components/Appointment/styles.scss';
+
+
+const classNames = require('classnames')
+
+export default function Appointment(props) {
+
+return(
+
+  <article className="appointment"></article>
+);
+}
