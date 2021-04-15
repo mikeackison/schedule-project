8c3e2ad1f15a30fecb11a5a49e70d959e2b66ef7
Mike Ackison <mike.ackison@shopify.com>
2021-04-15 18:37:38 -0400

    refactor interview list componenets, story working

--- stories/index.jsx
+++ stories/index.jsx
@@ -98,7 +98,7 @@ storiesOf('DayListItem', module) //Initiates Storybook and registers our DayList
           id={interviewer.id}
           name={interviewer.name}
           avatar={interviewer.avatar}
-          setInterviewer={action("setInterviewer")}
+          setInterviewer={event => action("setInterviewer")(interviewer.id)}
         />
       ));
 
