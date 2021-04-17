9fe26545753cda0e9eb6c8bf387fc42bd24717ab
Mike Ackison <mike.ackison@shopify.com>
2021-04-17 14:18:53 -0400

    add story for empty and booked appt usig fragment

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 import 'styles/styles.scss';
 
 
-const classNames = require('classnames')
+// const classNames = require('classnames')
 
 export default function Appointment(props) {
 
 return(
 
-  <article className="appointment"></article>
+  <article className="appointment">
+    {/* all appointments render header with time props */}
+    <Header time={props.time}/>
+    {/* if props interview is true show the appointment, otherwise show empty */}
+    { (props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/> }
+  </article>
 );
 }
