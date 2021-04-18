cf45b141595daa7b443252f1b3c9d9331816d329
Mike Ackison <mike.ackison@shopify.com>
2021-04-18 12:03:21 -0400

    update useeffect with promise.all for axois api calls, imported getAppointmentsForDay function using with the dailyAppointments varibable to map over to get appointmensts to recnder to Appointment componenet

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,61 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import DayList from 'components/DayList'
 import Appointment from 'components/Appointment';
+import { getAppointmentsForDay } from "../helpers/selectors"
 import axios from 'axios'
 
 import 'styles/Application.scss';
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "A. Student",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-
-  {
-    id: 4,
-    time: "3pm",
-  },
-
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "R. Andom",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  }
-];
-
-
 
 export default function Application(props) {
   
@@ -66,19 +15,22 @@ export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
-    days: []
+    days: [],
+    appointments: {}
   });
  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   // need to update the day and days state
   // pass a fucntion to setState in setDays
   const setDay = day => setState({ ...state, day });
-  const setDays = (days) => {
-    setState(prev => ({ ...prev, days }));;
-}
+  
+  // const setDays = (days) => {
+  //   setState(prev => ({ ...prev, days }));;
+  // }
 
 
-  const appointment = appointments.map(appointment => {
+  const appointment = dailyAppointments.map(appointment => {
     return(
   
       <Appointment
@@ -89,15 +41,33 @@ export default function Application(props) {
     );
   })
 
-  const daysURL = '/api/days'
-  useEffect(() =>{
-    axios.get(daysURL).then(response => {
-      setDays([...response.data])
-      console.log(response.data);
-    });
-  }, [] )
-  // empty dependency array when state changes use effect will execute
-  // empty dependency prevents infintite loop
+  // const daysURL = '/api/days'
+  // useEffect(() =>{
+  //   axios.get(daysURL).then(response => {
+  //     setDays([...response.data])
+  //     console.log(response.data);
+  //   });
+  // }, [] )
+  // // empty dependency array when state changes use effect will execute
+  // // empty dependency prevents infintite loop
+
+  useEffect(() => {
+    const GET_DAYS = '/api/days'
+    const GET_APPOINTMENTS = '/api/appointments'
+    const GET_INTERVIEWERS = '/api/interviewers'
+    Promise.all([
+      axios.get(GET_DAYS),
+      axios.get(GET_APPOINTMENTS),
+      axios.get(GET_INTERVIEWERS)
+    ]).then((all) => {
+      console.log(all)
+      console.log(all[2].data)
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    })
+  }, [])
+
+    
+
 
   return (
     <main className='layout'>
