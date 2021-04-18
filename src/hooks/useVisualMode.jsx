9dbce32e9677f507f61c9be61efc03ab7bedc636
Mike Ackison <mike.ackison@shopify.com>
2021-04-18 15:56:19 -0400

    transititon and back working, tests passing

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -2,10 +2,30 @@ import { useState } from 'react'
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) =>{
-    setMode(newMode)
-  }
+  // take in a new mode and update the mode state with the new value
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      setMode(newMode);
+    } else {
+      setMode(newMode);
 
-  return { mode, transition };
+      setHistory([...history, newMode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length <= 1) {
+      setMode(initial);
+    } else {
+      const newHistory = [...history.slice(0, -1)];
+
+      setHistory(newHistory);
+
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
 }
