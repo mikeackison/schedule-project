e98dc240bc8f70d5f9b3652dd47dd6f4fc96c2e1
Mike Ackison <mike.ackison@shopify.com>
2021-04-19 14:40:06 -0400

    basic validation funtion for form

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -8,8 +8,9 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   console.log("-------->", props.interviewer)
 
-  const [name, setName] = useState(props.name || "");
+  const [name, setName] = useState(props.name || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('')
 
   const reset = () => {
     setName('');
@@ -20,6 +21,22 @@ export default function Form(props) {
     reset();
     props.onCancel();
   }
+
+  const validate = () => {
+    if (name === '') {
+      setError('Student name cannot be blank')
+      return;
+    }
+    if (!interviewer) {
+      setError('Interviewer name cannot be blank');
+      return;
+    }
+
+    // clear error when validation passes
+    setError('');
+    props.onSave(name, interviewer);
+  }
+
   return(
 
   <main className="appointment__card appointment__card--create">
@@ -37,6 +54,7 @@ export default function Form(props) {
         */
       />
     </form>
+    <section className='appointment__validation'>{error}</section>
     <InterviewerList 
      interviewers={props.interviewers}
      value={interviewer}
@@ -46,7 +64,7 @@ export default function Form(props) {
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button onClick={cancel} danger>Cancel</Button>
-      <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+      <Button onClick={validate} confirm>Save</Button>
     </section>
   </section>
 </main>
