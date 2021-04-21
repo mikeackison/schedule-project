9fa71e2c0593294204e78193c32a488d6658d77d
Mike Ackison <mike.ackison@shopify.com>
2021-04-21 18:22:18 -0400

    fixed a spelling mistake

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -34,14 +34,14 @@ export default function Appointment(props) {
   );
 
   useEffect(() => {
-    if(mode === EMPTY && props.interview) {
+    if (mode === EMPTY && props.interview) {
       transition(SHOW)
     }
 
-    if(mode === SHOW && !props.interview) {
+    if (mode === SHOW && !props.interview) {
       transition(EMPTY)
     }
-  }, [props.interview, mode, transition]) 
+  }, [props.interview, mode, transition])
 
   const save = (name, interviewer) => {
 
@@ -51,72 +51,72 @@ export default function Appointment(props) {
     };
     transition(SAVING)
     props.bookInterview(props.id, interview)
-    .then(response => transition(SHOW))
-    .catch(error => transition(ERROR_SAVE, true));
+      .then(response => transition(SHOW))
+      .catch(error => transition(ERROR_SAVE, true));
   }
 
   const cancelInterview = () => {
 
     transition(DELETE, true)
     props.cancelInterview(props.id)
-    .then(() => transition(EMPTY))
-    .catch(error => transition(ERROR_DELETE, true));
+      .then(() => transition(EMPTY))
+      .catch(error => transition(ERROR_DELETE, true));
   }
 
 
-return(
+  return (
 
-  <article className='appointment'>
-    {/* all appointments render header with time props */}
-    <Header time={props.time}/>
+    <article className='appointment'>
+      {/* all appointments render header with time props */}
+      <Header time={props.time} />
 
-    {/* if props interview is true show the appointment, otherwise show empty */}
-    {/* need to update the props to incled interview */}
-    {/* { (props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/> } */}
-    {mode === EMPTY && !props.interview && <Empty onAdd={() =>transition(CREATE)} />}
-    {mode === SHOW && props.interview && (
-      
-      <Show
-        student={props.interview.student}
-        interviewer={props.interview.interviewer}
-        // onDelete={cancelInterview}
-        onDelete={() => transition(CONFIRM)}
-        onEdit={() => transition(EDIT)}
-      />
-    )}
-    {mode === CREATE && ( <Form onSave={save}  interviewers={props.interviewers} onCancel={() => back(EMPTY)} 
-      />
-    )}
-    {mode === EDIT && (<Form 
-      name={props.interview.student}
-      interviewers={props.interviewers}
-      interviewer={props.interview.interviewer.id}
-      student={props.student}
-      onSave={save}
-      onCancel={() => back()}
-      />
-    )}
-    {mode === SAVING && (<Status message='Saving'/>)}
-    {mode === DELETE && (<Status message='Deleteing' />)}
-    {mode === CONFIRM && (<Confirm 
-      message='Delete Appointment?' 
-      onConfirm={cancelInterview} 
-      onCancel={() => back()}
-    />
-    )}
-    {mode === ERROR_SAVE && (
-      <Error
-      message="There was an error saving your appointment"
-      onClose={back}
-      />
-    )}
+      {/* if props interview is true show the appointment, otherwise show empty */}
+      {/* need to update the props to incled interview */}
+      {/* { (props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/> } */}
+      {mode === EMPTY && !props.interview && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && props.interview && (
 
-    {mode === ERROR_DELETE && (
-      <Error 
-      message="There was an error deleteign your appoinment"
-      onClose={back}
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          // onDelete={cancelInterview}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
+        />
+      )}
+      {mode === CREATE && (<Form onSave={save} interviewers={props.interviewers} onCancel={() => back(EMPTY)}
       />
-    )}
-  </article>
-);
+      )}
+      {mode === EDIT && (<Form
+        name={props.interview.student}
+        interviewers={props.interviewers}
+        interviewer={props.interview.interviewer.id}
+        student={props.student}
+        onSave={save}
+        onCancel={() => back()}
+      />
+      )}
+      {mode === SAVING && (<Status message='Saving' />)}
+      {mode === DELETE && (<Status message='Deleting' />)}
+      {mode === CONFIRM && (<Confirm
+        message='Delete Appointment?'
+        onConfirm={cancelInterview}
+        onCancel={() => back()}
+      />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error
+          message="There was an error saving your appointment"
+          onClose={back}
+        />
+      )}
+
+      {mode === ERROR_DELETE && (
+        <Error
+          message="There was an error deleteign your appoinment"
+          onClose={back}
+        />
+      )}
+    </article>
+  );
 }
